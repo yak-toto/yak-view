7e74e358c3b956f1f07ca25e0c9d1e6273863c5a
Guillaume Le Pape <gui.lepape25@gmail.com>
2025-08-10 14:05:55 +0200

    Correct backend url setting
    Generate client code from yak-server 0.56.2

--- src/lib/hey-api.ts
+++ src/lib/hey-api.ts
@@ -1,8 +1,8 @@
 import type { CreateClientConfig } from './client/client.gen';
 
-import { BACKEND_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const createClientConfig: CreateClientConfig = (config) => ({
     ...config,
-    baseUrl: BACKEND_URL,
+    baseUrl: env.BACKEND_URL,
 });
